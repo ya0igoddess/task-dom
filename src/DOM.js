962adbebf9479b4dd5286202e98c580278a0d307
Daniil Genneke <genneke@mail.ru>
2022-03-24 22:14:29 +0300

    DOM

--- src/DOM.js
+++ src/DOM.js
@@ -5,6 +5,12 @@
   Считаем, что всегда передается тег, допускающий вставку текста в качестве своего содержимого (P, DIV, I и пр.).
 */
 export function appendToBody(tag, content, count) {
+    if (count > 0) {
+        let elem = document.createElement(tag);
+        elem.innerHTML = content;
+        document.body.appendChild(elem);
+        appendToBody(tag, content, count - 1);
+    }
 }
 
 /*
@@ -14,7 +20,14 @@ export function appendToBody(tag, content, count) {
   Каждый элемент должен иметь класс вида item_n, где n - глубина вложенности элемента. (Нумерацию ведем с единицы).
   Сформированное дерево верните в качестве результата работы функции.
 */
-export function generateTree(childrenCount, level) {
+export function generateTree(childrenCount, level, curLevel = 1) {
+    let div = document.createElement('div');
+    div.setAttribute('class', 'item_' + curLevel);
+    if (curLevel < level)
+        for (let i = 0; i < childrenCount; ++i) {
+            div.appendChild(generateTree(childrenCount, level, curLevel + 1));
+        }
+    return div;
 }
 
 /*
@@ -26,4 +39,14 @@ export function generateTree(childrenCount, level) {
   Сформированное дерево верните в качестве результата работы функции.
 */
 export function replaceNodes() {
+    let tree = generateTree(2, 3);
+    for (let elem of tree.getElementsByClassName('item_2')) {
+        let section = document.createElement('section');
+        section.setAttribute('class', 'item_2');
+        while (elem.firstChild) {
+            section.appendChild(elem.firstChild);
+        }
+        elem.parentNode.replaceChild(section, elem);
+    }
+    return tree;
 }
